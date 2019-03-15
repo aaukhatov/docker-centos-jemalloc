5cca17b3da323e6b0477ce9ddae21a0686574dda
Arthur Aukhatov <aaukhatov@gmail.com>
2019-03-15 15:32:35 +0300

    Update jeprof-reports.sh

--- jeprof-reports.sh
+++ jeprof-reports.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
-jeprof --svg /tmp/jeprof.* > /tmp/jeprof-report.svg 2>/dev/null
-jeprof --text /tmp/jeprof.* > /tmp/jeprof-report.txt 2>/dev/null
+jeprof --svg /tmp/jeprof.* > /tmp/jeprof/jeprof-report.svg 2>/dev/null
+jeprof --text /tmp/jeprof.* > /tmp/jeprof/jeprof-report.txt 2>/dev/null
